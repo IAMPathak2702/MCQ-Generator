446014cd8bbb3498f5f2075e17eb460fe47f98ce
Ved Praksh <vp.ved.vpp@gmail.com>
2024-05-12 01:56:02 +0530

    intailising OPENAI LLM

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages , setup
+
+setup(
+    name='mcq_generator',
+    
+    version='1.0.0',
+    
+    description="""The OpenAI MCQ Generator package revolutionizes content creation by seamlessly
+    automating the generation of multiple-choice questions (MCQs) across various domains. Leveraging
+    advanced natural language processing (NLP) algorithms, this innovative tool swiftly generates
+    high-quality MCQs tailored to specific topics or subjects. With its user-friendly interface, 
+    educators, content creators, and developers can effortlessly craft quizzes, exams, and study 
+    materials with diverse question formats and varying difficulty levels. The package not only 
+    saves time and resources but also ensures the authenticity and relevance of the generated 
+    questions through rigorous validation processes. Additionally, its adaptive capabilities 
+    allow for continuous improvement and customization, empowering users to deliver engaging 
+    and effective learning experiences effortlessly.""",
+    
+    author="Ved Prakash Pathak",
+    
+    author_email="vp.ved.vpp@gmail.com",
+    
+    packages=find_packages(),
+        
+    
+)
